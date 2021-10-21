c1f9b4b6f597f6fd877efdbd3d9323f86b495e94
jijun.lei <jijun.lei@mybogames.com>
2021-10-21 15:15:21 +0800

    0

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   env: {
     node: true,
+
   },
   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
@@ -10,6 +11,6 @@ module.exports = {
   rules: {
     // "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     // "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-unused-vars":"off"
+    "no-unused-vars": "off"
   },
 };
